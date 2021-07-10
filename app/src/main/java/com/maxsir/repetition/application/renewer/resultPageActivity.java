da23a93e325b972c2e1584028045b7083a9d1f90
Maxim <syromolotov.maks@mail.ru>
2021-07-10 16:06:56 +0300

    Added some features - hide/show -default/common intent action service filter by checkbox added

--- app/src/main/java/com/maxsir/repetition/application/renewer/resultPageActivity.java
+++ app/src/main/java/com/maxsir/repetition/application/renewer/resultPageActivity.java
@@ -3,12 +3,14 @@ package com.maxsir.repetition.application.renewer;
 import androidx.appcompat.app.AppCompatActivity;
 
 //import com.maxsir.repetition.application.renewer.Copier;
+import android.annotation.SuppressLint;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
 
@@ -23,12 +25,17 @@ public class resultPageActivity extends AppCompatActivity {
 
     }
 
+    private String receivedTextByIntent;
+
+    @SuppressLint("SetTextI18n")
     public void onGetActivityIntent(){
         Intent intent=getIntent();
         String string=intent.getStringExtra(SWITCH_NAME_OF_INTENT);
         TextView tv=(TextView) findViewById(R.id.textViewResultPageSender);
         tv.setText(string);
         copy(tv,string);
+        receivedTextByIntent=string;
+        tv.setText(tv.getText().toString()+"\nSuccessfully Copied To Clipboard");
     }
 
     public void copy(TextView tv, String result){
@@ -39,16 +46,32 @@ public class resultPageActivity extends AppCompatActivity {
     }
 
     public void SendByMessangerOnClick(View view) {
-        TextView tv=(TextView) findViewById(R.id.textViewResultPageSender);
         CheckBox checkBox=(CheckBox) findViewById(R.id.checkBoxResultSender);
+        CheckBox checkBoxService=(CheckBox) findViewById(R.id.checkBox2);
         if(checkBox.isChecked()){
             Intent intent = new Intent(Intent.ACTION_SEND);
             intent.setType("text/plain");
-            intent.putExtra(Intent.EXTRA_TEXT,tv.getText());
-            Intent chooser = Intent.createChooser(intent, getString(R.string.shareText));
-            startActivity(chooser);
-            //startActivity(intent);
+            intent.putExtra(Intent.EXTRA_TEXT,receivedTextByIntent);
+            if(!checkBoxService.isChecked()){
+                Intent chooser = Intent.createChooser(intent, getString(R.string.shareText));
+                startActivity(chooser);
+            }else {
+                startActivity(intent);
+            }
         }
 
+    }
+
+    public void stateChanged(View view) {
+        Button tv=(Button) findViewById(R.id.button);
+        CheckBox checkBox=(CheckBox) findViewById(R.id.checkBox2);
+        CheckBox checkBoxMainSelected=(CheckBox) findViewById(R.id.checkBoxResultSender);
+        if(!checkBoxMainSelected.isChecked()){
+            checkBox.setVisibility(View.INVISIBLE);
+            tv.setVisibility(View.INVISIBLE);
+        }else{
+            checkBox.setVisibility(View.VISIBLE);
+            tv.setVisibility(View.VISIBLE);
+        }
     }
 }
