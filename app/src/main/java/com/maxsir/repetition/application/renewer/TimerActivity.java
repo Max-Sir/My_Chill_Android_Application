7db2159d80a17ab8ad2401f4856dd1ed0ac327bb
Maxim <syromolotov.maks@mail.ru>
2021-07-11 15:31:53 +0300

    now StopWatch uses Milliseconds and provide logging of some actions

--- app/src/main/java/com/maxsir/repetition/application/renewer/TimerActivity.java
+++ app/src/main/java/com/maxsir/repetition/application/renewer/TimerActivity.java
@@ -1,9 +1,9 @@
 package com.maxsir.repetition.application.renewer;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.media.MediaPlayer;
+import android.annotation.SuppressLint;
+import android.app.Instrumentation;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
@@ -20,26 +20,29 @@ public class TimerActivity extends AppCompatActivity {
      * @param started
      *      used by methods to understand even this started by method onStart();
      */
-    private int seconds=0;
+    private int milliseconds =0;
     private boolean running;
     private boolean playingMusic;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if(savedInstanceState != null){
-            seconds=savedInstanceState.getInt("seconds");
+            milliseconds =savedInstanceState.getInt("seconds");
             running=savedInstanceState.getBoolean("running");
             runTimerService();
         }
         setContentView(R.layout.activity_timer);
     }
 
+    private boolean isStarted(){
+        return started;
+    }
 
     private boolean isRunning(){
         return running;
     }
     public void onStart(View view) {
-        if(running||started){
+        if(isRunning()||isStarted()){
             return;
         }
         started=true;
@@ -49,7 +52,7 @@ public class TimerActivity extends AppCompatActivity {
     }
 
     public void onReset(View view) {
-        seconds=0;
+        milliseconds =0;
         started=false;
         running=false;
         handler.removeCallbacksAndMessages(null);
@@ -59,13 +62,13 @@ public class TimerActivity extends AppCompatActivity {
 
     public void onStop(View view) {
         Button btn = findViewById(R.id.stop);
-        if(started==true) {
+        if(isStarted()) {
             running = false;
             started= false;
             btn.setText(getString(R.string.resume));
             handler.removeCallbacksAndMessages(null);
         }
-        else if(started==false){
+        else {
             started=true;
             running=true;
             btn.setText(getString(R.string.stop));
@@ -84,23 +87,28 @@ public class TimerActivity extends AppCompatActivity {
         handler.post(new Runnable() {
             @Override
             public void run() {
-                int hours=seconds/3600;
-                int minutes=(seconds%3600)/60;
-                int secondsIsOnTimer=seconds%60;
+                int seconds= milliseconds /100;
+                int hours= seconds /3600;
+                int minutes=(seconds %3600)/60;
+                int secondsIsOnTimer= seconds%60;
+                int ms= milliseconds %1_00;
+
 
                 String time=String.format(Locale.getDefault(),"%02d:%02d:%02d",hours,minutes,secondsIsOnTimer);
-                tv.setText(time);
+                @SuppressLint("DefaultLocale") String displayedTime=time+String.format(".%02d",ms%100);
+                tv.setText(displayedTime);
 
                 if(running){
-                    seconds++;
+                    milliseconds++;
                 }
 
-                if(seconds%2==0){
+                if(milliseconds %200==0){
                     Log.d("Music started at ",time);
                     //MediaPlayer mp=MediaPlayer.create(R.raw.file);
                 }
 
-                handler.postDelayed(this,1000);
+
+                handler.postDelayed(this,1);
             }
         });
     }
@@ -108,7 +116,8 @@ public class TimerActivity extends AppCompatActivity {
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putInt("seconds",seconds);
+        outState.putInt("seconds", milliseconds);
         outState.putBoolean("running",running);
+       // Instrumentation instrumentation=new Instrumentation();
     }
 }
