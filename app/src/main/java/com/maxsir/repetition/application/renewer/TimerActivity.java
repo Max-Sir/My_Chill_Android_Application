394308fae069b1b1325f476490c81dfc72b2f70e
Maxim <syromolotov.maks@mail.ru>
2021-07-11 14:41:09 +0300

    last version with seconds

--- app/src/main/java/com/maxsir/repetition/application/renewer/TimerActivity.java
+++ app/src/main/java/com/maxsir/repetition/application/renewer/TimerActivity.java
@@ -6,13 +6,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.Locale;
 
 public class TimerActivity extends AppCompatActivity {
 
+    private boolean started;
+    /**
+     * @param started
+     *      used by methods to understand even this started by method onStart();
+     */
     private int seconds=0;
     private boolean running;
     private boolean playingMusic;
@@ -32,9 +39,10 @@ public class TimerActivity extends AppCompatActivity {
         return running;
     }
     public void onStart(View view) {
-        if(running==true){
+        if(running||started){
             return;
         }
+        started=true;
         running=true;
         handler.removeCallbacksAndMessages(null);
         runTimerService();
@@ -42,6 +50,7 @@ public class TimerActivity extends AppCompatActivity {
 
     public void onReset(View view) {
         seconds=0;
+        started=false;
         running=false;
         handler.removeCallbacksAndMessages(null);
         TextView tv=findViewById(R.id.timerView);
@@ -49,8 +58,21 @@ public class TimerActivity extends AppCompatActivity {
     }
 
     public void onStop(View view) {
-        running=false;
-        handler.removeCallbacksAndMessages(null);
+        Button btn = findViewById(R.id.stop);
+        if(started==true) {
+            running = false;
+            started= false;
+            btn.setText(getString(R.string.resume));
+            handler.removeCallbacksAndMessages(null);
+        }
+        else if(started==false){
+            started=true;
+            running=true;
+            btn.setText(getString(R.string.stop));
+            runTimerService();
+        }
+
+
     }
 
     final Handler handler=new Handler();
@@ -73,7 +95,8 @@ public class TimerActivity extends AppCompatActivity {
                     seconds++;
                 }
 
-                if(seconds%15==0){
+                if(seconds%2==0){
+                    Log.d("Music started at ",time);
                     //MediaPlayer mp=MediaPlayer.create(R.raw.file);
                 }
 
